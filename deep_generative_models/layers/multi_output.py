9b58f9b018de17e44a683a9d11bb759108e1ca3c
Ramiro Camino <ramirocamino@gmail.com>
2020-03-19 13:45:55 +0100

    Nicer block building in the multi-output layer avoiding the use of the type string check.

--- deep_generative_models/layers/multi_output.py
+++ deep_generative_models/layers/multi_output.py
@@ -8,7 +8,7 @@ from torch.nn.functional import gumbel_softmax, softmax
 
 from torch.distributions.one_hot_categorical import OneHotCategorical
 
-from deep_generative_models.metadata import Metadata
+from deep_generative_models.metadata import Metadata, VariableMetadata
 
 
 class OutputBinaryVariableActivation(Module):
@@ -39,6 +39,33 @@ class OutputCategoricalVariableActivation(Module):
             return OneHotCategorical(logits=inputs).sample()
 
 
+class BlockBuilder:
+    variable_metadata: VariableMetadata
+    activation: Optional[Module]
+    size: int
+
+    def __init__(self, variable_metadata: VariableMetadata) -> None:
+        if not (variable_metadata.is_binary() or variable_metadata.is_numerical()):
+            raise Exception("Unexpected variable type '{}' for block.".format(variable_metadata.get_type()))
+
+        self.variable_metadata = variable_metadata
+        self.size = 0
+        self.add(variable_metadata)
+
+    def matches_type(self, variable_metadata: VariableMetadata) -> bool:
+        return self.variable_metadata.get_type() == variable_metadata.get_type()
+
+    def add(self, variable_metadata: VariableMetadata) -> None:
+        assert self.matches_type(variable_metadata)
+        self.size += 1
+
+    def build(self, input_size: int) -> Module:
+        if self.variable_metadata.is_binary():
+            return Sequential(Linear(input_size, self.variable_metadata.get_size()), OutputBinaryVariableActivation())
+        elif self.variable_metadata.is_numerical():
+            return Linear(input_size, self.variable_metadata.get_size())
+
+
 class MultiOutputLayer(Module):
     layers: ModuleList
 
@@ -48,29 +75,31 @@ class MultiOutputLayer(Module):
         self.layers = ModuleList()
 
         # accumulate binary or numerical variables into "blocks"
-        current_block_type = None
-        current_block_size = 0
+        current_block = None
 
         for variable_metadata in metadata.get_by_variable():
             # first check if a block needs to be created
-            if current_block_size > 0 and variable_metadata.get_type() != current_block_type:
+            if current_block is not None and not current_block.matches_type(variable_metadata):
                 # create the block
-                self._add_block(current_block_type, current_block_size, input_size)
-                # empty the accumulated data
-                current_block_type = None
-                current_block_size = 0
+                self.layers.append(current_block.build(input_size))
+                # empty the block
+                current_block = None
 
             # if it is a binary or numerical variable
             if variable_metadata.is_binary() or variable_metadata.is_numerical():
-                assert variable_metadata.get_size() == 1
-                current_block_type = variable_metadata.get_type()
-                current_block_size += 1
+                # create a block
+                if current_block is None:
+                    current_block = BlockBuilder(variable_metadata)
+                # or add to the existing block
+                else:
+                    current_block.add(variable_metadata)
 
             # if it is a categorical variable
             elif variable_metadata.is_categorical():
                 # create the categorical layer
-                self._add_layer(input_size, variable_metadata.get_size(),
-                                OutputCategoricalVariableActivation(temperature))
+                self.layers.append(Sequential(
+                    Linear(input_size, variable_metadata.get_size()),
+                    OutputCategoricalVariableActivation(temperature)))
 
             # if it is another type
             else:
@@ -78,24 +107,9 @@ class MultiOutputLayer(Module):
                     variable_metadata.get_type(), variable_metadata.get_name()))
 
         # if there is still accumulated data for a block
-        if current_block_size > 0:
+        if current_block is not None:
             # create the last block
-            self._add_block(current_block_type, current_block_size, input_size)
-
-    def _add_layer(self, input_size: int, output_size: int, activation: Optional[Module] = None) -> None:
-        if activation is None:
-            layer = Linear(input_size, output_size)
-        else:
-            layer = Sequential(Linear(input_size, output_size), activation)
-        self.layers.append(layer)
-
-    def _add_block(self, block_type: str, block_size: int, input_size: int) -> None:
-        if block_type == "binary":
-            self._add_layer(input_size, block_size, OutputBinaryVariableActivation())
-        elif block_type == "numerical":
-            self._add_layer(input_size, block_size)
-        else:
-            raise Exception("Unexpected block type '{}'.".format(block_type))
+            self.layers.append(current_block.build(input_size))
 
     def forward(self, inputs: Tensor) -> Tensor:
         return torch.cat([layer(inputs) for layer in self.layers], dim=1)
