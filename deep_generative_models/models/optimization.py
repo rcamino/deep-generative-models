41ff63bda488392e9ac4e34856856fdd6acffebd
Ramiro Camino <ramirocamino@gmail.com>
2020-03-22 16:53:17 +0100

    Optimizer factory.

---
+++ deep_generative_models/models/optimization.py
@@ -0,0 +1,33 @@
+from torch.optim import Adam, SGD
+from torch.optim.optimizer import Optimizer
+
+from typing import Dict
+
+from deep_generative_models.architecture import Architecture
+from deep_generative_models.configuration import Configuration
+
+
+Optimizers = Dict[str, Optimizer]
+
+
+optimizer_classes_by_name = {
+    # PyTorch optimizers (could add more)
+    "Adam": Adam,
+    "SGD": SGD,
+}
+
+
+def create_optimizers(architecture: Architecture, optimizers_configuration: Configuration) -> Optimizers:
+    optimizers = {}
+    for optimizer_name, optimizer_configuration in optimizers_configuration.items():
+        # extract the module parameters
+        parameters = []
+        for module_name in optimizer_configuration.modules:
+            module = architecture[module_name]
+            parameters.extend(module.parameters())
+        # create the optimizer
+        arguments = optimizer_configuration.get("arguments", default=[], transform_default=False)
+        keyword_arguments = optimizer_configuration.get("keyword_arguments", default={}, transform_default=False)
+        optimizer_class = optimizer_classes_by_name[optimizer_configuration.factory]
+        optimizers[optimizer_name] = optimizer_class(parameters, *arguments, **keyword_arguments)
+    return optimizers
