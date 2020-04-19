b82bd0750b288b55337e31af0cc5f94d38170bbd
Ramiro Camino <ramirocamino@gmail.com>
2020-04-19 14:36:06 +0200

    The serial task runner contains the paths to other tasks like the multi-processing runner.

--- deep_generative_models/tasks/serial_runner.py
+++ deep_generative_models/tasks/serial_runner.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from deep_generative_models.configuration import Configuration
+from deep_generative_models.configuration import Configuration, load_configuration
 from deep_generative_models.tasks.task import Task
 
 
@@ -12,5 +12,5 @@ class SerialTaskRunner(Task):
     def run(self, configuration: Configuration) -> None:
         from deep_generative_models.tasks.runner import TaskRunner  # import here to avoid circular dependency
         task_runner = TaskRunner()
-        for child_configuration in configuration.tasks:
-            task_runner.run(child_configuration)
+        for child_configuration_path in configuration.tasks:
+            task_runner.run(load_configuration(child_configuration_path))
